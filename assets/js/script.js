ef2017117a036b48c31362aed74e40c80a969dbc
Paula <payevenes@uc.cl>
2021-05-30 18:16:15 -0400

    add background color change to navbar on scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,12 @@
 $(function () {
+    $(document).scroll(function () {
+        var $nav = $(".navbar");
+        var $picture = $("#carouselExampleControls");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $picture.height());
+    });
+});
+
+$(function () {
     $("#clickComenzar").click(function () {
         var email = prompt("Ingresa tu correo");
         alert("Te contactaremos pronto");
